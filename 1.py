698a77e61e58846aa1de64e5afa5b3225961afc3
Tao Liu <tao.liu@liverpool.ac.uk>
2022-09-03 21:55:19 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,15 +1,7 @@
-from traceback import print_tb
-
-
-a = 10
-b = 20
-c = a +b 
-print(a)
-print(b)
-print(c)
-
-
-numbers = [1,2,3,4,5]
-area_calc = [x*x for x in numbers]
-print(area_calc)
-print("done")
+import numpy
+import pandas as pd
+myarray = numpy.array([[1, 2, 3], [4, 5, 6]])
+rownames = ['a', 'b']
+colnames = ['one', 'two', 'three']
+mydataframe = pd.DataFrame(myarray, index=rownames, columns=colnames)
+print(mydataframe)
