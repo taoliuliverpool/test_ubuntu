309e7d0377272d3e2d2d52f0938ba0c8fdb5ebd5
Tao Liu <tao.liu@liverpool.ac.uk>
2022-09-03 21:13:53 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,15 @@
+from traceback import print_tb
+
+
 a = 10
 b = 20
 c = a +b 
+print(a)
+print(b)
 print(c)
+
 
 numbers = [1,2,3,4,5]
 area_calc = [x*x for x in numbers]
 print(area_calc)
+print("done")
